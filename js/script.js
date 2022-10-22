6f7a622ff45a9fe9b587c0d441c54a006d6f80a4
Konrad Piatek <konrad.jerzy.piatek@gmail.com>
2022-10-22 19:33:18 +0200

    add function getMoveName

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
+function getMoveName(MoveId){
+    if (MoveId == 1) {
+        return 'rock';
+    } else if (MoveId == 2) {
+        return 'paper';
+    } else if (MoveId == 3) {
+        return 'scissors';
+    } else {
+        return 'unknow movemenet';
+    }
+}
+
 // Computer Move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 printMessage('random number ' + randomNumber);
-let computerMove = 'unknown movement';
-if(randomNumber == 1) {
+let computerMove = getMoveName(randomNumber);
+/*if(randomNumber == 1) {
     computerMove = 'rock';
 } else if(randomNumber == 2) {
     computerMove = 'paper';
 } else if(randomNumber == 3) {
     computerMove = 'scissors';
-}
+}*/
 console.log('Computer move: ' + computerMove);
 // Player Move
 let playerInput = prompt('Choose your move! 1: stone, 2: paper, 3: scissors.')
 console.log('player choose: ' + playerInput);
 
-let playerMove = 'unknown movement';
-if(playerInput == 1) {
+let playerMove = getMoveName(playerInput);
+/*if(playerInput == 1) {
     playerMove = 'rock';
 } else if (playerInput == 2) {
     playerMove = 'paper';
@@ -24,6 +36,6 @@ if(playerInput == 1) {
 } else {
     playerMove = 'unknow movemenet';
 }
+*/
 console.log('Player move: ' + playerMove);
-
 
